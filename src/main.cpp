e6b0452b9ddce23caefa6b2549fff5d758ca1c68
Daniel López <danieljlopez@udec.cl>
2022-06-10 03:22:27 -0400

    Dic add

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "TrieArray.h"
 #include "TrieMap.h"
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[])
+{
     TrieArray *trieArray = new TrieArray();
     TrieMap *trieMap = new TrieMap();
     // Keys to be inserted in Trie
@@ -9,20 +10,35 @@ int main(int argc, char const *argv[]) {
                        "any", "by", "bye", "their", "these"};
 
     // Inserting keys in Trie Map
-    for (auto &key: keys) {
+    for (auto &key : keys)
+    {
         trieMap->insert(key);
     }
 
-    trieMap->remove("these");
+    trieMap->remove("a");
     cout << trieMap->search("a") << endl;
     trieMap->insert("a");
-
 
     // Displaying content of Trie
     cout << "Content of TrieMap: " << endl;
     vector<string> list = trieMap->getAll();
-    for (auto &word: list) {
+    for (auto &word : list)
+    {
         cout << word << endl;
+    }
+
+    // Leer archivo
+    ifstream ifile;
+    ifile.open("dic/words.txt"); // Abrir dic
+    if (ifile.is_open())
+    {
+        string s;
+        ifile >> s;
+        cout << "de archivo:" << s << endl;
+    }
+    else
+    {
+        cout << "No se pudo abrir el diccionario" << endl;
     }
 
     delete trieMap;
